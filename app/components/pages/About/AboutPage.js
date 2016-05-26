dbbbba7a21d16c7affc71256f1a2e027c6a6fc31
whroman <me@walterroman.com>
2016-05-27 01:30:49 +0200

    shortening about again. bumping package version. contains build files.

--- app/components/pages/About/AboutPage.js
+++ app/components/pages/About/AboutPage.js
@@ -26,12 +26,12 @@ export class About extends Component {
                 <hr />
                 <span>{ 'All sentences are randomly generated. Learn more about how this works by looking at ' }</span>
                 <a href='https://github.com/whroman/conjugate/blob/gh-pages/README.md'>
-                    { 'the README of this project\'s open-source codebase' }
+                    { 'this project\'s open-source codebase' }
                 </a>
                 <span>{ '.' }</span>
                 <br />
                 <br />
-                <span>{ 'The daily background image is National Geographic\'s photo of the day.' }</span>
+                <span>{ 'The daily background image is NatGeo\'s photo of the day.' }</span>
                 <br />
                 <br />
                 <span>{ 'Questions? Comments? Email ' }</span>
