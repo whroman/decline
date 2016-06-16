a7ae98f0166f3888ed68bb49b462c0c900a8ac4f
whroman <me@walterroman.com>
2016-06-17 00:37:06 +0200

    Adding many more accusative beginnings.

--- generator/randomPhrase/accusativeBeginnings.js
+++ generator/randomPhrase/accusativeBeginnings.js
@@ -1,19 +1,53 @@
-export default [
-    'Ich mag',
-
-    'Ich möchte',
-    'Du möchtest',
-    'Er möchte',
-    'Sie möchte',
-    'Wir möchten',
-    'Ihr möchtet',
-    'Sie möchten',
-
-    'Ich habe',
-    'Du hast',
-    'Er hat',
-    'Sie hat',
-    'Wir haben',
-    'Ihr habt',
-    'Sie haben'
+const generator = (set) => [
+    'Ich '  + set[0],
+    'Du '   + set[1],
+    'Er '   + set[2],
+    'Sie '  + set[3],
+    'Wir '  + set[4],
+    'Ihr '  + set[5],
+    'Sie '  + set[6],
 ];
+
+const accusativeBeginnings = generator([
+    'möchte',
+    'möchtest',
+    'möchte',
+    'möchte',
+    'möchten',
+    'möchtet',
+    'möchten'
+]).concat(generator([
+    'will',
+    'willst',
+    'will',
+    'will',
+    'wollen',
+    'wollt',
+    'wollen'
+])).concat(generator([
+    'habe',
+    'hast',
+    'hat',
+    'hat',
+    'haben',
+    'habt',
+    'haben',
+])).concat(generator([
+    'sehe',
+    'siehst',
+    'sieht',
+    'sieht',
+    'sehen',
+    'seht',
+    'sehen',
+])).concat(generator([
+    'höre',
+    'hörst',
+    'hört',
+    'hört',
+    'höben',
+    'hört',
+    'höben',
+]));
+
+export default accusativeBeginnings;
