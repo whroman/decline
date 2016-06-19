9cec99501850470408f799e7f8a9f312c388ffa7
whroman <me@walterroman.com>
2016-06-19 15:57:05 +0200

    Build to reintroduce "production" js and resolve navigation issues

--- tasks/build.webpack.config.js
+++ tasks/build.webpack.config.js
@@ -3,13 +3,24 @@
 const webpack = require('webpack');
 
 const webpackConfig = require('./dev.webpack.config.js');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-webpackConfig.plugins.push(
+const GA_NAMESPACE = 'DECLINE_GOOGLE_ANALYTICS_TOKEN';
+const plugins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            'NODE_ENV': '"production"',
+            [GA_NAMESPACE]: JSON.stringify(process.env[GA_NAMESPACE])
+        },
+    }),
+    new ExtractTextPlugin('build.css'),
     new webpack.optimize.UglifyJsPlugin({
         compress: {
             warnings: false
         }
     })
-);
+];
+
+Object.assign(webpackConfig, { plugins });
 
 module.exports = webpackConfig;
