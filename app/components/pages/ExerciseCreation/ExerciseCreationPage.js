8f616367394287d0be2ebf1f1d89b13471ab501c
whroman <me@walterroman.com>
2016-06-23 01:01:42 +0200

    shitty table is in place. need to display data in more understandable way (group by article type. since that's what results in different tables)

--- app/components/pages/ExerciseCreation/ExerciseCreationPage.js
+++ app/components/pages/ExerciseCreation/ExerciseCreationPage.js
@@ -2,8 +2,21 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import { create, load } from 'app/dux/adjectiveTrainer';
-
 import CreationForm from './CreationForm';
+
+import foo from 'tables/conjugationTable/data';
+import articleTypes from 'tables/articles/types/data';
+import genders from 'tables/nouns/genders/data';
+
+console.log(foo);
+const tables = foo.adjSuffixes.list.reduce((memo, { articleType, grammarCase, objectGender, text }) => {
+    const processedArticleType = (articleType === '3') ? '1' : articleType;
+    if (!memo[objectGender]) memo[objectGender] = [];
+    if (!memo[objectGender][grammarCase]) memo[objectGender][grammarCase] = [];
+    if (!memo[objectGender][grammarCase][processedArticleType]) memo[objectGender][grammarCase][processedArticleType] = text;
+    return memo;
+}, []);
+console.log(tables);
 
 export class ExerciseCreationPage extends Component {
 
@@ -30,6 +43,37 @@ export class ExerciseCreationPage extends Component {
             <div>
                 <div className='row'>
                     <div className='modal column small-10 small-centered  '>
+                        <br/>
+                        <div>
+                            <div className='row'>
+                                <div className='row-header'></div>
+                                { tables[0].map((col1, col1Index) => (
+                                    <div className='col-header'>
+                                        <div className='col1-header'>{ col1Index }</div>
+                                        { col1.map((col2, col2Index) => (
+                                            <div className='col2-header'>{ articleTypes[col2Index].translations.deu }</div>
+                                        )) }
+                                    </div>
+                                )) }
+                            </div>
+
+                            { tables.map((row, rowI) => (
+                                <div>
+                                    <div className='row'>
+                                        <div className='row-header'>{ genders[rowI] }</div>
+                                        { row.map((col1) => (
+                                            <div className='col1'>
+                                                { col1.map((col2) => (
+                                                    <div className='col2'>
+                                                        { col2 }
+                                                    </div>
+                                                )) }
+                                            </div>
+                                        )) }
+                                    </div>
+                                </div>
+                            )) }
+                        </div>
                         <br/>
                         <div className='row'>
                             <div className='column small-11 small-centered'>
