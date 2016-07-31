1b2d3e7f904c8053e1ef1e1c5e554e143fd6e3a9
whroman <me@walterroman.com>
2016-07-31 12:16:04 +0200

    added more verb exercises

--- tables/rawExercises/verbsWithPrepositions.js
+++ tables/rawExercises/verbsWithPrepositions.js
@@ -4,48 +4,369 @@ export default [
         stubs: [1, 2, 3],
         translations: {
             eng: 'Tina works at a shoe factory'
-        }
+        },
+        tags: [
+            'arbeiten',
+            'bei'
+        ]
     },
     {
         text: 'Der Tischler arbeitet an einem Möbelstück',
         stubs: [2, 3, 4],
         translations: {
             eng: 'The carpenter is working on a piece of furniture'
-        }
+        },
+        tags: [
+            'arbeiten',
+            'an'
+        ]
     },
     {
         text: 'Du musst mir bei dieser Arbeit helfen',
         stubs: [2, 3, 4, 6],
         translations: {
             eng: 'You have to help me with this work'
-        }
+        },
+        tags: [
+            'helfen',
+            'bei'
+        ]
     },
     {
         text: 'Meine Tante hilft meiner Schwester bei ihren Hausaufgaben',
         stubs: [2, 3, 5, 6],
         translations: {
             eng: 'My aunt helps my sister with her homework'
-        }
+        },
+        tags: [
+            'helfen',
+            'bei'
+        ]
     },
     {
         text: 'Der Vater hat sich bei der Schule informiert',
         stubs: [3, 4, 5, 7],
         translations: {
             eng: 'The father was informed by the school'
-        }
+        },
+        tags: [
+            'informieren',
+            'bei'
+        ]
     },
     {
         text: 'Wir sind über den Unfall informiert',
         stubs: [2, 3, 5],
         translations: {
             eng: 'We are informed about the accident'
-        }
+        },
+        tags: [
+            'informieren',
+            'über'
+        ]
     },
     {
         text: 'Ich bleibe bei meiner Meinung',
         stubs: [1, 2, 3],
         translations: {
             eng: 'I stand by my opinion'
-        }
-    }
+        },
+        tags: [
+            'bleiben',
+            'bei'
+        ]
+    },
+    {
+        text: 'Du musst noch beim Reisebüro anrufen',
+        stubs: [3, 5],
+        translations: {
+            eng: 'You must still call the travel agency'
+        },
+        tags: [
+            'anrufen',
+            'bei'
+        ]
+    },
+    {
+        text: 'Ich spreche mit deiner Schwester',
+        stubs: [2, 3],
+        translations: {
+            eng: 'I am speaking with your sister'
+        },
+        tags: [
+            'sprechen',
+            'mit'
+        ]
+    },
+    {
+        text: 'Wir müssen über den Zeitplan sprechen',
+        stubs: [2, 3, 5],
+        translations: {
+            eng: 'We need to talk about the schedule'
+        },
+        tags: [
+            'sprechen',
+            'über'
+        ]
+    },
+    {
+        text: 'Sie hat von dir gesprochen',
+        stubs: [2, 3, 4],
+        translations: {
+            eng: 'She has spoken of you'
+        },
+        tags: [
+            'sprechen',
+            'von'
+        ]
+    },
+    {
+        text: 'Ich freue mich auf die Reise',
+        stubs: [2, 3, 4],
+        translations: {
+            eng: 'I\'m looking forward to the trip'
+        },
+        tags: [
+            'freuen',
+            'auf'
+        ]
+    },
+    {
+        text: 'Ich habe mich auf mein neues Leben gefreut',
+        stubs: [2, 3, 4, 5, 7],
+        translations: {
+            eng: 'I was looking forward to my new life.'
+        },
+        tags: [
+            'freuen',
+            'auf'
+        ]
+    },
+    {
+        text: 'Bei der Wohnungssuche hat mir ein Bekannter geholfen',
+        stubs: [0, 1, 7],
+        translations: {
+            eng: 'An acquaintance helped me with the house hunting.'
+        },
+        tags: [
+            'helfen',
+            'bei'
+        ]
+    },
+    {
+        text: 'Ich warte seit Monaten auf den Besuch meiner besten Freundin',
+        stubs: [1, 4, 5, 7, 8],
+        translations: {
+            eng: 'I have been waiting for months for my best friend\'s visit.'
+        },
+        tags: [
+            'warten',
+            'auf'
+        ]
+    },
+    {
+        text: 'Er spricht von den Nachrichten',
+        stubs: [1, 2],
+        translations: {
+            eng: 'He speaks about the news.'
+        },
+        tags: [
+            'sprechen',
+            'von'
+        ]
+    },
+    {
+        text: 'Zeit, dass ich an meine Ferien denke',
+        stubs: [3, 4, 6],
+        translations: {
+            eng: 'Time for me to think about my holiday.'
+        },
+        tags: [
+            'denken',
+            'an'
+        ]
+    },
+    {
+        text: 'Ich sitze hier rum ganz allein in der Kälte',
+        stubs: [1, 6, 7],
+        translations: {
+            eng: 'I sit around here all alone in the cold.'
+        },
+        tags: [
+            'sitzen',
+            'in'
+        ]
+    },
+    {
+        text: 'Ich möchte mich für deine Hilfe bedanken',
+        stubs: [2, 3, 4, 6],
+        translations: {
+            eng: 'I would like to thank you for your help.'
+        },
+        tags: [
+            'bedanken',
+            'für'
+        ]
+    },
+    {
+        text: 'Wir möchten uns bei Ihnen bedanken',
+        stubs: [3, 4, 6],
+        translations: {
+            eng: 'We would like to thank you (formal).'
+        },
+        tags: [
+            'bedanken',
+            'bei'
+        ]
+    },
+    {
+        text: 'Wir mussten nach der Adresse fragen',
+        stubs: [2, 3, 5],
+        translations: {
+            eng: 'We had to ask for the address.'
+        },
+        tags: [
+            'fragen',
+            'nach'
+        ]
+    },
+    {
+        text: 'Kann ich dich um Rat fragen',
+        stubs: [2, 3, 4, 6],
+        translations: {
+            eng: 'Can I ask you for advice?'
+        },
+        tags: [
+            'fragen',
+            'um'
+        ]
+    },
+    {
+        text: 'Die Touristen fragen einen Passanten nach dem Weg',
+        stubs: [2, 5, 6],
+        translations: {
+            eng: 'The tourists are asking a passerby for directions.'
+        },
+        tags: [
+            'fragen',
+            'nach'
+        ]
+    },
+    {
+        text: 'Aus deinem Verhalten schließe ich, dass du schuld bist',
+        stubs: [0, 1, 3],
+        translations: {
+            eng: 'From your behavior I conclude that you are to blame.'
+        },
+        tags: [
+            'schließen',
+            'aus'
+        ]
+    },
+    {
+        text: 'Die Ereigniße der Zukunft können wir nicht aus den gegenwärtigen erschließen',
+        stubs: [7, 8, 10],
+        translations: {
+            eng: 'We can\'t infer the events of the future from those of the present.'
+        },
+        tags: [
+            'erschließen',
+            'aus'
+        ]
+    },
+    {
+        text: 'Auch auf dem höchsten Thron sitzt man auf dem eigenen Hintern',
+        stubs: [1, 2, 3, 5, 7, 8],
+        translations: {
+            eng: 'Even on the highest throne, you still sit on your own butt.'
+        },
+        tags: [
+            'sitzen',
+            'auf'
+        ]
+    },
+    {
+        text: 'Ich lerne Deutsch auf der Straße',
+        stubs: [1, 3, 4],
+        translations: {
+            eng: 'I learn German on the street.'
+        },
+        tags: [
+            'lernen',
+            'auf'
+        ]
+    },
+
+
+
+
+
+
+    {
+        text: 'Ich wollte die Blumen auf das Fensterbrett stellen',
+        stubs: [4, 5, 7],
+        translations: {
+            eng: 'I wanted to put the flowers on the windowsill.'
+        },
+        tags: [
+            'stellen',
+            'auf'
+        ]
+    },
+    {
+        text: 'Die Buntstiften sind auf den Boden gefallen',
+        stubs: [3, 4, 6],
+        translations: {
+            eng: 'The crayons fell to the ground.'
+        },
+        tags: [
+            'fallen',
+            'auf'
+        ]
+    },
+    {
+        text: 'Sie können Ihren Mantel an die Garderobe hängen',
+        stubs: [4, 5, 7],
+        translations: {
+            eng: 'You (formal) can hang your coat on the coat rack.'
+        },
+        tags: [
+            'lernen',
+            'an'
+        ]
+    },
+    {
+        text: 'Die Blumen liegen auf dem Fensterbrett',
+        stubs: [2, 3, 4],
+        translations: {
+            eng: 'The flowers are on the windowsill.'
+        },
+        tags: [
+            'liegen',
+            'auf'
+        ]
+    },
+    {
+        text: 'Das Papier liegt unter dem Tisch',
+        stubs: [2, 3, 4],
+        translations: {
+            eng: 'The paper is under the table.'
+        },
+        tags: [
+            'lernen',
+            'unter'
+        ]
+    },
+    {
+        text: 'Du kannst die Papiere in meine Schublade legen',
+        stubs: [4, 5, 7],
+        translations: {
+            eng: 'You can place the papers in my drawer.'
+        },
+        tags: [
+            'liegen',
+            'in'
+        ]
+    },
+
 ];
