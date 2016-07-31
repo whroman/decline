7a715e8332604a69150df9b85415798c3ce69783
whroman <me@walterroman.com>
2016-07-31 12:15:25 +0200

    Added translations to verb exercises.

--- app/components/pages/Verbs/VerbsPage.js
+++ app/components/pages/Verbs/VerbsPage.js
@@ -42,7 +42,7 @@ class VerbWord extends Component {
         if (text !== answer) {
             word.push(
                 <input
-                    className={ 'verb-word-input' }
+                    className={ 'VerbWord-input' }
                     type='text'
                     maxLength={ answer.length }
                     size={ answer.length }
@@ -56,7 +56,7 @@ class VerbWord extends Component {
 
         if (index > 0) word.unshift(<span> </span>);
 
-        return (<span>{ word }</span>);
+        return (<span className='VerbWord'>{ word }</span>);
     }
 
     handleInputChange (event, expectedValue) {
@@ -64,56 +64,74 @@ class VerbWord extends Component {
         const isCorrect = value.toLowerCase() === expectedValue.toLowerCase();
         const isFilled = value.length === expectedValue.length;
         this.setState({ isCorrect, isFilled });
-        console.log(this.state);
     }
 
 }
 
-class VerbSentence extends Component {
+class VerbExercise extends Component {
 
     constructor() {
         super();
         this.state = {
-            showTooltip: false
+            showTranslationTooltip: false,
+            showAnswerTooltip: false
         };
     }
 
     render() {
-        const { wordList, translations, index } = this.props;
+        const { text, wordList, translations, index } = this.props;
 
         return (
-            <div className='VerbSentence'>
+            <div className='VerbExercise'>
 
-                <Tooltip show={ this.state.showTooltip } >
+                <Tooltip show={ this.state.showTranslationTooltip } >
                     <div>{ translations.eng }</div>
                 </Tooltip>
 
-                <span>{ `${index + 1 }.` }</span>
+                <Tooltip show={ this.state.showAnswerTooltip } >
+                    <div>{ text }</div>
+                </Tooltip>
 
-                <i
-                    onMouseOver={ this.handleMouseOver.bind(this) }
-                    onMouseOut={ this.handleMouseOut.bind(this) }
-                    className='wr-ico wr-ico-info-circle wr-ico-fw'
-                />
+                <div className='VerbExercise-details'>
+                    <span>{ `${index + 1 }.` }</span>
 
-                {
-                    wordList.map((word, wordIndex) => (
-                        <VerbWord { ...Object.assign(word, { index: wordIndex }) } />
-                    ))
-                }
-                <span>{ '.' }</span>
+                    <i
+                        onMouseOver={ this.showTranslationTooltip.bind(this, true) }
+                        onMouseOut={ this.showTranslationTooltip.bind(this, false) }
+                        className='wr-ico wr-ico-language wr-ico-fw'
+                    />
+
+                    <i
+                        onMouseOver={ this.showAnswerTooltip.bind(this, true) }
+                        onMouseOut={ this.showAnswerTooltip.bind(this, false) }
+                        className='wr-ico wr-ico-unlock-alt wr-ico-fw'
+                    />
+                </div>
+
+                <div className='VerbExercise-sentence'>
+                    {
+                        wordList.map((word, wordIndex) => (
+                            <VerbWord { ...Object.assign(word, { index: wordIndex }) } />
+                        ))
+                    }
+                    <span>{ '.' }</span>
+                </div>
             </div>
         );
     }
 
-    handleMouseOver() {
-        console.log('in')
-        this.setState({ showTooltip: true });
+    showTranslationTooltip(showTranslationTooltip) {
+        this.setState({
+            showTranslationTooltip,
+            showAnswerTooltip: false
+        });
     }
 
-    handleMouseOut() {
-        console.log('out')
-        this.setState({ showTooltip: false });
+    showAnswerTooltip(showAnswerTooltip) {
+        this.setState({
+            showAnswerTooltip,
+            showTranslationTooltip: false
+        });
     }
 
 }
@@ -143,7 +161,7 @@ export class About extends Component {
                 <hr />
                 {
                     forViewConsumption.map((item, index) => (
-                        <VerbSentence { ...Object.assign(item, { index }) } />
+                        <VerbExercise { ...Object.assign(item, { index }) } />
                     ))
                 }
             </div>
